a0c6ea01a9f62ed04b40f7a3799f58740dbed09a
Hamza khan <hamzakhancs15@gmail.com>
2019-07-13 22:03:17 +0400

    View created and preview controller setup

--- FaceDetectorSelfie/FaceDetectorSelfie/Controllers/PreviewViewController.swift
+++ FaceDetectorSelfie/FaceDetectorSelfie/Controllers/PreviewViewController.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class PreviewViewController: UIViewController {
 
+    var image : UIImage!
+    var cgImage : CGImage!
+    @IBOutlet var imgView : UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imgView.image = image
+        
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
